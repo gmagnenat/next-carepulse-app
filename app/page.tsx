f1fcefc4fd36cf1124f8c917d7bc9e2400341b2a
Gwenaël Magnenat <g.magnenat@gmail.com>
2024-07-07 23:16:37 +0200

    Add customformfield component and start building the homepage form

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,43 @@
-import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
+
+import PatientForm from "@/components/forms/PatientForm";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl underline text-white">Home</h1>
-      <Button>Click me</Button>
+    <div className="flex h-screen max-h-screen">
+      {/* TODO: OTP Verification | PassKey modal */}
+
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[495px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            width={1000}
+            height={1000}
+            alt="patient"
+            className="mb-12 h-10 w-max"
+          />
+
+          <PatientForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 Carepulse
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
